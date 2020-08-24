43cf36587ee915875b86d2648344eb67d8c62087
rajajamal <aceh.rajajamal@gmail.com>
2020-08-24 23:19:00 +0700

    just finishing dashboard

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+// @desc Login/Landing Page
+// @route GET /
+
+router.get("/", (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
+});
+
+// @desc Dasboard
+// @route GET /dasboard
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+module.exports = router;
